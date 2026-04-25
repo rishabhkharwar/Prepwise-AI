0a694c7f86b5b8bb4d1013d424801a14fc08def1
Rishabh Kharwar <rishabhkharwar674@gmail.com>
2026-04-25 14:28:02 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://prepwise-ai-74cx.onrender.com",
     withCredentials: true,
 })
 
